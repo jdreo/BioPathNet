b85eefb6b9fa80f78c13526f63ada6dfd5746381
Claire Laudy <claire.laudy@pasteur.fr>
2026-02-25 15:50:49 +0100

    feat(Container): building a containered version of BioPathNet.
    Adds a apptainer .def file for container building.
    Adds a bin/build_container_BPN.sh file to launch the building of the container.

---
+++ bin/build_container_BPN.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+set -o pipefail
+
+# Go to the repository
+cd $(dirname $0)/..
+
+if [[ ! -f "BioPathNet.def" ]] ; then
+    echo "ERROR: the given path does not point to a BioPathNet repository." >&2
+    exit 2
+fi
+
+if command -v module ; then
+    module load apptainer
+fi
+apptainer cache clean -f
+REV=$(git_rev)
+apptainer build -F biopathnet.sif biopathnet.def
+
+
