77ba6c10a6fdc6f0b17c674506ca5ff39bd4198f
Johann Dreo <johann.dreo@pasteur.fr>
2026-03-03 16:21:54 +0100

    fix(apptainer): use --nv when running + more deps + venv in PATH

--- bin/apptainer_run.sh
+++ bin/apptainer_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-module load ninja cuda
-srun -p gpu -q gpu --gres=gpu:1 apptainer run -B /pasteur/appa/homes/claudy --bind $(pwd):/BioPathNet biopathnet.sif script/run.py config/mock/mockdata_run.yaml [0]
+module load apptainer ninja cuda gcc
+srun -p gpu -q gpu --gres=gpu:1 apptainer run --nv --bind $HOME --bind $(pwd):/BioPathNet --bind /opt/gensoft:/opt/gensoft:ro biopathnet.sif script/run.py config/mock/mockdata_run.yaml [0]
