bf16528439319b7a47406fa3db5dfbf20f0d4dcd
Claire  LAUDY <claire.laudy@pasteur.fr>
2026-03-26 14:13:23 +0100

    feat(apptainer config): add a config file to run the apptnainer with mockdata as an example.

--- bin/srun_apptainer.sh
+++ bin/srun_apptainer.sh
@@ -6,7 +6,7 @@ cat config/mock/mockdata_run.yaml | sed "s,/Users/claudy/work/projects/BioPathNe
 cmd="srun \
     -p gpu \
     -q gpu \
-    --gres=gmem:50G,gpu:1 \
+    --gres=gpu:1 \
     --mem=50G \
     apptainer \
         run \
@@ -14,7 +14,8 @@ cmd="srun \
             --writable-tmpfs \
             --cleanenv \
             --bind $HOME --bind $(pwd):/BioPathNet \
-            biopathnet.sif -f run -c config/mock/mockdata_run_apptainer.yaml --gpus [0] $@"
+            biopathnet.sif $@"
+            # biopathnet.sif -f run -c config/mock/mockdata_run_apptainer.yaml --gpus [0] $@"
 
 echo "Submitting:" >&2
 echo $cmd >&2
