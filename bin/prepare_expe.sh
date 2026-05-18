c9da71a99d048a47e05d2f69c66caf0eb85553a9
Johann Dreo <johann.dreo@pasteur.fr>
2026-05-18 15:52:36 +0200

    feat(bin): a script to prepare an expe

---
+++ bin/prepare_expe.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $(basename $0) <input_conf> <input_datadir>" >&2
+    echo "This will use the current working directory for outputs." >&2
+    exit 2
+fi
+
+
+module load apptainer cuda
+
+input_conf="$1"
+shift
+
+input_datadir="$1"
+shift
+
+work_dir="$(pwd)"
+expe_conf="config_$(date -Iseconds).yaml"
+
+bpn_dir="$(realpath $(dirname $input_conf)/../../)"
+if [ ! -f ${bpn_dir}/pyproject.toml ] ; then
+    echo "ERROR: the directory inferred from the config file path does not contains BioPathNet: '$bpn_dir'"
+    exit 3
+fi
+if [ ! -f $bpn_dir/biopathnet.sif ] ; then
+    echo "ERROR: I cannot find a 'biopathnet.sif' container in inferred BioPathNet directory: '${bpn_dir}'" >&2
+    echo "Build it with 'bin/build_apptainer.sh' from your BioPathNet directory."
+    exit 4
+fi
+# Link the container.
+ln -s $bpn_dir/biopathnet.sif
+
+# Instantiate the config file.
+cat $input_conf | sed "s,{{DATA_DIR}},${input_datadir},g" | sed "s,{{OUTPUT_DIR}},${work_dir},g" > ${expe_conf}
+
+
+echo "Config:" >&2
+cat ${expe_conf}
+
