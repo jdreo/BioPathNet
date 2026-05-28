b06adda812133900f5a261d46e7638ca073e6fbd
Johann Dreo <johann.dreo@pasteur.fr>
2026-05-28 15:15:21 +0200

    feat(prepare): allow to pass several config files to prepare.sh

--- bin/prepare_expe.sh
+++ bin/prepare_expe.sh
@@ -3,26 +3,24 @@
 set -e
 set -o pipefail
 
-if [ $# -ne 2 ] ; then
-    echo "Usage: $(basename $0) <input_conf> <input_datadir>" >&2
+if [ $# -lt 2 ] ; then
+    echo "Usage: $(basename $0) <input_datadir> <input_confs...>" >&2
     echo "This will use the current working directory for outputs." >&2
     exit 2
 fi
 
-
 module load apptainer cuda
-
-input_conf="$1"
-shift
 
 # Need realpath for the underlying BPN run in a container that mounts the CWD.
 input_datadir="$(realpath $1)"
 shift
 
-work_dir="$(pwd)"
-expe_conf="config_$(date -Iseconds).yaml"
+first_input_conf="$1"
+conf_dir="$(realpath $(dirname $first_input_conf))"
 
-conf_dir="$(realpath $(dirname $input_conf))"
+work_dir="$(pwd)"
+timestamp="$(date -Iseconds | tr : -)"
+
 bpn_dir="${conf_dir%BioPathNet/*}BioPathNet/"
 if [ ! -f ${bpn_dir}/pyproject.toml ] ; then
     echo "ERROR: the directory inferred from the config file path does not contains BioPathNet: '$bpn_dir'"
@@ -37,9 +35,13 @@ fi
 ln -s -f $bpn_dir/biopathnet.sif
 
 # Instantiate the config file.
-cat $input_conf | sed "s,{{DATA_DIR}},${input_datadir},g" | sed "s,{{OUTPUT_DIR}},${work_dir},g" > ${expe_conf}
-
+for input_conf in "$@" ; do
+    name=$(basename "$input_conf")
+    stem="${name%.*}"
+    ext="${name##*.}"
+    cat $input_conf | sed "s,{{DATA_DIR}},${input_datadir},g" | sed "s,{{OUTPUT_DIR}},${work_dir},g" > "${work_dir}/${stem}_${timestamp}.${ext}"
+done
 
 echo "Config:" >&2
-cat ${expe_conf}
+ls -1 *${timestamp}*
 
